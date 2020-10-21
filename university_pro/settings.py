d0675d22782be633e244c6729796615ef3b1ddae
GanapathiAmbore <ganapathi.ambore@globaldata.com>
2020-10-21 19:56:40 +0530

    changed settings

--- university_pro/settings.py
+++ university_pro/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'university_pro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'university',
+        'USER':'root',
+        'PASSWORD':'root'
     }
 }
 
